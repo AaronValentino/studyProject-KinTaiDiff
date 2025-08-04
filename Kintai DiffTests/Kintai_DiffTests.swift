642a58f2c08175d28ed8834b19936f3740cd9f83
Aaron Valentino <140781889+AaronValentino@users.noreply.github.com>
2025-08-04 12:09:02 +0900

    Initial commit

---
+++ Kintai DiffTests/Kintai_DiffTests.swift
@@ -0,0 +1,17 @@
+//
+//  Kintai_DiffTests.swift
+//  Kintai DiffTests
+//
+//  Created by アーロンヴァレンティノ on 2025/08/04.
+//
+
+import Testing
+@testable import Kintai_Diff
+
+struct Kintai_DiffTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
